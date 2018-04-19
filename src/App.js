42eca05f274cacee6f9453f6dc1006e9b2580b16
ZhekaiJin <jinzhekai0705@163.com>
2018-04-19 00:01:21 -0400

    unstash for eject

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {createStore} from 'redux'
+import {Button} from "antd-mobile"
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 class App extends Component {
   render() {
     return (
@@ -10,6 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Messenger</h1>
         </header>
+        <Button onClick={()=>console.log("Hello")}>Hello</Button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
